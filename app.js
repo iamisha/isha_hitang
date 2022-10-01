7dec4ff327864f854b54d804ba664d3ca273eb87
iamisha <ishahitang18@outlook.com>
2022-10-01 23:50:22 +0545

    full code of portfolio written

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
-/*
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
 
-function PageTransition(){
-    //Button click active class
-    for(let i = 0; i < sectBtn.length; i++){
-        sectBtn[i].addEventListener('click', function(){
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += 'active-btn';
-        })
-    }
-}
-
-PageTransition();
-*/
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
